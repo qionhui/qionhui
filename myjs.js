52a0401809fc77fb6f5ef965a1247ac381dd3938
qionhui <40323604+qionhui@users.noreply.github.com>
2018-10-06 20:29:09 -0700

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -2,12 +2,12 @@ this.Link = document.getElementById("Text");
 this.l = 0.00;
 this.fx = 0;
 function a(){
-	if(l >= 80)fx = 1;
+	if(l >= 83.5)fx = 1;
 	if(l <= 0)fx = 0;
 	if(fx == 0)
-		l += 0.06;
+		l += 0.07;
 	else if(fx == 1)
-		l -= 0.06;
+		l -= 0.07;
 	Link.style.left = l + "%";
 }
 
